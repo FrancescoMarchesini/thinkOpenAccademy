c47fc9fc16b122aa7ca2a157b8873706da10b1f0
FrancescoMarchesini <marchesinifrancesco1@gmail.com>
2018-10-19 19:29:34 +0200

    aggiunto esercizio

---
+++ app_00_Exercise/02_funzioni/03_array_reverse.js
@@ -0,0 +1,33 @@
+/**
+ * 0. Invertire l'ordine degli elementi di un array, scrivendo la funzione **reverseArray** 
+ * che accetta come argomneto un vettore  e produce un nuovo vettore che contiente gli stessi 
+ * elementi in ordine in verso. **reverersArrayInPlace** fa la stessa cosa ma senza creare un nuovo 
+ * vettore. Alla fine fai il calcolo di quale dei due è piu veloce
+ */
+
+range = (start, stop, step) =>{
+    let res = [];
+
+    if (step == 0){
+        step = 1;
+    }else if(step > 0){
+        for( let i = start; i < stop; i = i + step ){
+            res.push(i);
+        }
+    }else if(step < 0){
+        step = step * (-1);
+        for( let i = stop; i > start; i = i - step ){
+            res.push(i);
+        }
+    }
+
+    return res;
+}
+
+ reversArray = (array) =>{
+
+    return array;
+ }
+
+ let array = range(0, 50, 2);
+ console.log(`il vettore di partenza ${array} ed il suo reverso ${reversArray(array)}`);
